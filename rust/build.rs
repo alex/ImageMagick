2f0f01ec49a86307e269e78f98df9076e3b90308
Alex Gaynor <alex.gaynor@gmail.com>
2020-12-31 16:26:57 -0500

    missing files

---
+++ rust/build.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg("-I../")
+        .blacklist_item("FP_.*")
+        .size_t_is_usize(true)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Error generating bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Error writing bindings");
+}
---
+++ rust/wrapper.h
@@ -0,0 +1,6 @@
+#include "MagickCore/studio.h"
+#include "MagickCore/magick.h"
+#include "MagickCore/module.h"
+
+// Workaround bindgen limitation.
+const size_t BindingsMagickImageCoderSignature = MagickImageCoderSignature;
