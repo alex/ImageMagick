763e619092889b6bdaac2e692054177e667fb267
Alex Gaynor <alex.gaynor@gmail.com>
2021-01-07 16:11:57 -0500

    handle new CLI args correctly

--- rust/build.rs
+++ rust/build.rs
@@ -2,11 +2,20 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let mut clang_args = vec!["-I../".to_string()];
+    if let Ok(cflags) = env::var("MAGICK_CLFAGS") {
+        // XXX: THIS IS WRONG AND BROKEN IF YOU HAVE A PATH A SPACE OR ANYTHING
+        // LIKE THAT. USE A REAL PARSER HERE.
+        for flag in cflags.split(' ') {
+            clang_args.push(flag.to_string());
+        }
+    }
+
     println!("cargo:rerun-if-changed=wrapper.h");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-I../")
+        .clang_args(clang_args)
         .blacklist_item("FP_.*")
         .size_t_is_usize(true)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
